386a2e69a1d3f89bc9ff033c20c4a5f3922b5941
Felipe Macedo <fmacedo746@gmail.com>
2022-04-08 15:51:37 -0300

    contants folder added

---
+++ lib/constants/textConstants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const defaultTextStyle = TextStyle(
+  fontFamily: "Fredoka",
+  fontStyle: FontStyle.normal,
+  fontSize: 12,
+  color: Color(0xFFFFFFFF),
+);
