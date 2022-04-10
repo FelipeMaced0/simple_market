e02275d5f012f580e6ea33ab25903a116a76277f
Felipe Macedo <fmacedo746@gmail.com>
2022-04-10 20:00:47 -0300

    feature: adaptable added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/",
+      initialRoute: "/Home",
       routes: {
         "/": (context) => const Login(),
         "/Home": (context) => Home(),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_market/constants/text_constants.dart';
+import 'package:simple_market/widgets/card_img.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -15,38 +16,25 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.fromLTRB(0, 60, 0, 0),
+            margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
             child: Column(
               children: [
                 Text(
                   "Olá User!",
                   style: defaultTextStyle.copyWith(
-                      fontSize: 32, color: Color(0xFFCFCACA)),
+                      fontSize: 32, color: const Color(0xFFCFCACA)),
                 ),
                 Text(
                   "Aproveite as melhores ofertas!",
                   style: defaultTextStyle.copyWith(
-                      fontSize: 16, color: Color(0xFFFF9675)),
+                      fontSize: 16, color: const Color(0xFFFF9675)),
                 ),
-                Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 140,
-                        height: 100,
-                        child: Image.asset(
-                          "images/nrd-D6Tu_L3chLE-unsplash.jpg",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 140,
-                        height: 60,
-                      )
-                    ],
-                  ),
-                )
+                const CardImg(
+                  width: 140,
+                  height: 100,
+                  quarterTurns: 0,
+                  child: SizedBox(width: 140, height: 30,),
+                ),
               ],
             ),
           )
