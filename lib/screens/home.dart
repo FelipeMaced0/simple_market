f8feb84456312316389fb5129c507ee3942b6631
Felipe Macedo <fmacedo746@gmail.com>
2022-04-10 22:57:29 -0300

    feature: margin attribute added to card_img

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:simple_market/constants/text_constants.dart';
 import 'package:simple_market/widgets/card_img.dart';
 
+const double padding_card = 30;
+
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
 
@@ -13,32 +15,82 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-            child: Column(
-              children: [
-                Text(
-                  "Olá User!",
-                  style: defaultTextStyle.copyWith(
-                      fontSize: 32, color: const Color(0xFFCFCACA)),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Olá User!",
+                style: defaultTextStyle.copyWith(
+                    fontSize: 32, color: const Color(0xFFCFCACA)),
+              ),
+              Text(
+                "Aproveite as melhores ofertas!",
+                style: defaultTextStyle.copyWith(
+                    fontSize: 16, color: const Color(0xFFFF9675)),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        CardImg(
+                          width: 140,
+                          height: 100,
+                          quarterTurns: 0,
+                          margin: EdgeInsets.only(bottom: padding_card),
+                          child: SizedBox(
+                            width: 140,
+                            height: 30,
+                          ),
+                        ),
+                        CardImg(
+                          width: 140,
+                          height: 100,
+                          quarterTurns: 0,
+                          margin: EdgeInsets.only(bottom: padding_card),
+                          child: SizedBox(
+                            width: 140,
+                            height: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        CardImg(
+                          width: 140,
+                          height: 100,
+                          quarterTurns: 0,
+                          margin: EdgeInsets.only(bottom: padding_card),
+                          child: SizedBox(
+                            width: 140,
+                            height: 30,
+                          ),
+                        ),
+                        CardImg(
+                          width: 140,
+                          height: 100,
+                          quarterTurns: 0,
+                          margin: EdgeInsets.only(bottom: padding_card),
+                          child: SizedBox(
+                            width: 140,
+                            height: 30,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                Text(
-                  "Aproveite as melhores ofertas!",
-                  style: defaultTextStyle.copyWith(
-                      fontSize: 16, color: const Color(0xFFFF9675)),
-                ),
-                const CardImg(
-                  width: 140,
-                  height: 100,
-                  quarterTurns: 0,
-                  child: SizedBox(width: 140, height: 30,),
-                ),
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
