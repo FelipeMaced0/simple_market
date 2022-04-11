80b3ca13c365d150ae2f01c3087ee91977e0277c
Felipe Macedo <fmacedo746@gmail.com>
2022-04-11 11:21:17 -0300

    Registration screen added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/Home",
+      initialRoute: "/Registration",
       routes: {
         "/": (context) => const Login(),
         "/Home": (context) => const Home(),
--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:simple_market/widgets/logo.dart';
 import 'package:simple_market/widgets/input.dart';
@@ -14,32 +16,56 @@ class _RegistrationState extends State<Registration> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: ListView(
           children: [
-            Logo(
-              assetName: "images/logo.svg",
-              imageColor: Color(0xFFFF9675),
-            ),
-            InputField(
-              label: "Nome:",
-            ),
-            InputField(
-              label: "Email:",
-            ),
-            InputField(
-              label: "Endereço:",
-            ),
-            Row(
-              children: const [
-                InputField(
-                  label: "Cep:",
-                  width: 150,
-                ),
-                InputField(
-                  label: "Número:",
-                  width: 150,
-                ),
-              ],
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Logo(
+                    assetName: "images/logo.svg",
+                    imageColor: Color(0xFFFF9675),
+                  ),
+                  InputField(
+                    label: "Nome:",
+                  ),
+                  InputField(
+                    label: "Email:",
+                  ),
+                  InputField(
+                    label: "Endereço:",
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      InputField(
+                        label: "Cep:",
+                        width: 130,
+                      ),
+                      InputField(
+                        label: "Número:",
+                        width: 130,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 20, top: 20),
+                        child: TextButton(
+                          onPressed: () => {},
+                          child: Text("Registrar"),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Color(0xFFFF9675),
+                            fixedSize: Size(140, 30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
