b0b59ff9c8711271f2c2651ea985fd71bd23c721
Felipe Macedo <fmacedo746@gmail.com>
2022-04-12 16:19:01 -0300

    feat: General Screen working

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/Home",
+      initialRoute: "/General",
       routes: {
         "/": (context) => const Login(),
         "/Home": (context) => const Home(),
--- lib/screens/general.dart
+++ lib/screens/general.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_market/constants/text_constants.dart';
 import 'package:simple_market/widgets/card_img.dart';
+import 'package:simple_market/widgets/footer_bar.dart';
 
 const double padding_card = 30;
 
@@ -15,11 +16,52 @@ class _GeneralState extends State<General> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+      extendBodyBehindAppBar: true,
+      bottomNavigationBar: FooterBar(
+        margin: const EdgeInsets.only(top: 10),
+        padding: const EdgeInsets.only(top: 5),
+        children: [
+          IconFooter(
+            icon: Icons.home_filled,
+            iconSize: 20,
+            iconColor: const Color(0xFFFFFFFF),
+            text: "Início",
+            textStyle: defaultTextStyle.copyWith(
+              fontSize: 8,
+            ),
+          ),
+          IconFooter(
+            icon: Icons.format_list_numbered,
+            iconSize: 20,
+            iconColor: const Color(0xFFFFFFFF),
+            text: "Minha Lista",
+            textStyle: defaultTextStyle.copyWith(
+              fontSize: 8,
+            ),
+          ),
+          IconFooter(
+            icon: Icons.store_mall_directory_rounded,
+            iconSize: 20,
+            iconColor: const Color(0xFFFFFFFF),
+            text: "Buscar",
+            textStyle: defaultTextStyle.copyWith(
+              fontSize: 8,
+            ),
+          ),
+          IconFooter(
+            icon: Icons.account_circle_sharp,
+            iconSize: 20,
+            iconColor: const Color(0xFFFFFFFF),
+            text: "Minha Conta",
+            textStyle: defaultTextStyle.copyWith(
+              fontSize: 8,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
                 "Geral!",
@@ -31,89 +73,54 @@ class _GeneralState extends State<General> {
                 style: defaultTextStyle.copyWith(
                     fontSize: 16, color: const Color(0xFFFF9675)),
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 30),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                        CardImg(
-                          width: 140,
-                          height: 100,
-                          quarterTurns: 0,
-                          margin: EdgeInsets.only(bottom: padding_card),
-                          child: SizedBox(
-                            width: 140,
-                            height: 30,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-
-                ),
-              )
+              const SizedBox(
+                height: 20,
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                mainAxisSpacing: 25,
+                crossAxisSpacing: 20,
+                crossAxisCount: 2,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                children: const [
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                  CardImg(
+                    width: 140,
+                    height: 100,
+                    quarterTurns: 0,
+                    child: Text("data"),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
