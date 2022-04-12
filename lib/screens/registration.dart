22f0431e1b6ef80a217aeb2aa4a66cbcf90e7a1f
Felipe Macedo <fmacedo746@gmail.com>
2022-04-11 21:29:27 -0300

    Registration Screen is ready

--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:simple_market/widgets/circular_image.dart';
 import 'package:simple_market/widgets/logo.dart';
 import 'package:simple_market/widgets/input.dart';
+import 'package:simple_market/constants/text_constants.dart';
 
 class Registration extends StatefulWidget {
   const Registration({Key? key}) : super(key: key);
@@ -26,43 +28,71 @@ class _RegistrationState extends State<Registration> {
                   Logo(
                     assetName: "images/logo.svg",
                     imageColor: Color(0xFFFF9675),
+                    imageHeight: 40,
+                  ),
+                  CircularImage(
+                    name: "images/franki-chamaki-ivfp_yxZuYQ-unsplash.jpg",
+                    color: Color(0xFFD2CECC),
+                    xAxis: 150.0,
+                    yAxis: 150.0,
+                    margin: EdgeInsets.all(30),
                   ),
                   InputField(
                     label: "Nome:",
+                    textStyle: defaultTextStyle.copyWith(
+                      color: Color(0xFFFF9675),
+                    ),
                   ),
                   InputField(
                     label: "Email:",
+                    textStyle: defaultTextStyle.copyWith(
+                      color: Color(0xFFFF9675),
+                    ),
                   ),
                   InputField(
                     label: "Endereço:",
+                    textStyle: defaultTextStyle.copyWith(
+                      color: Color(0xFFFF9675),
+                    ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
+                    children:  [
                       InputField(
                         label: "Cep:",
+                        textStyle: defaultTextStyle.copyWith(
+                          color: Color(0xFFFF9675),
+                        ),
                         width: 130,
                       ),
                       InputField(
                         label: "Número:",
+                        textStyle: defaultTextStyle.copyWith(
+                          color: Color(0xFFFF9675),
+                        ),
                         width: 130,
                       ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 20, top: 20),
-                        child: TextButton(
+                  Container(
+                    margin: EdgeInsets.only(right: 20.0, top: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
                           onPressed: () => {},
-                          child: Text("Registrar"),
+                          child: Text("Registrar",
+                            style: defaultTextStyle.copyWith(
+                              fontSize: 15,
+                            ),
+                          ),
                           style: TextButton.styleFrom(
                             backgroundColor: Color(0xFFFF9675),
                             fixedSize: Size(140, 30),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
