07d6b6c15a94e939e2b8e5fed27ffda6819acd7d
Felipe Macedo <fmacedo746@gmail.com>
2022-04-14 12:33:59 -0300

    Feature: color's constant file

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBaseOrange = Color(0xFFFF5722);
+const Color kLightOrange = Color(0xFFFF9675);
+const Color kBaseGreen = Color(0xFF83E61F);
+const Color kBaseGrey = Color(0xFFD2CECC);
+const Color kLightGrey = Color(0xFFF5F4F4);
